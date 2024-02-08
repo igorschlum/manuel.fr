b6eced3120040d99b0d82671f0a87dc0f138d514
Igor Schlumberger <igor@schlum.com>
2024-02-08 19:45:48 +0100

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/igor/Dave/Ollama/ollama.fr/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/igor/Dave/Ollama/ollama.fr/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/igor/Dave/Ollama/ollama.fr/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/igor/Dave/Ollama/ollama.fr/src/css/custom.css"),
-];
