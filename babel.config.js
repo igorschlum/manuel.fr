05fec975cbcba5279feb1f3300584622e0119246
Igor Schlumberger <igor@schlum.com>
2024-11-17 22:21:47 +0100

    Merci Phind.com
    J'avais une erreur très bizarre qui a été corrigée par Phind.com après une trentaine de conseils.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
-module.exports = {
+/* module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+};
+
+*/
+module.exports = function(api) {
+  api.cache(true);
+  
+  return {
+    presets: [
+      require.resolve('@docusaurus/core/lib/babel/preset'),
+      ['@babel/preset-env', { modules: false }]
+    ],
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      '@babel/plugin-transform-class-properties',
+      // Autres plugins si nécessaire
+    ],
+  };
 };
